64ce68babf79c9a56c246aa8f32e67d51de6d491
dmetasoul01 <opensource@dmetasoul.com>
2022-05-30 20:15:23 +0800

    restore pg port number in dev env (#25)

--- src/main/scala/com/dmetasoul/lakesoul/meta/DBConfig.java
+++ src/main/scala/com/dmetasoul/lakesoul/meta/DBConfig.java
@@ -3,7 +3,7 @@ package com.dmetasoul.lakesoul.meta;
 public abstract class DBConfig {
 
     static String driver = "org.postgresql.Driver";
-    static String url = "jdbc:postgresql://127.0.0.1:5432/test_lakesoul_meta?stringtype=unspecified";
+    static String url = "jdbc:postgresql://127.0.0.1:5433/test_lakesoul_meta?stringtype=unspecified";
     static String username = "yugabyte";
     static String password = "yugabyte";
 
