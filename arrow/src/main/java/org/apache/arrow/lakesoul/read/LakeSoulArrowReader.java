b89a0fe8de159e857173b0401433105821ebcabb
zenghua <441651826@qq.com>
2022-07-14 11:18:23 +0800

    [Arrow][LakeSoulArrowReader] Create LakeSoulArrowReader JNI

---
+++ arrow/src/main/java/org/apache/arrow/lakesoul/read/LakeSoulArrowReader.java
@@ -0,0 +1,71 @@
+package org.apache.arrow.lakesoul.read;
+
+import org.apache.arrow.c.ArrowArray;
+import org.apache.arrow.c.ArrowSchema;
+import org.apache.arrow.c.Data;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.VectorSchemaRoot;
+
+import java.util.HashMap;
+
+public class LakeSoulArrowReader {
+    /**
+     * native method for creating LakeSoulArrowReader
+     * @param files
+     * @param primary_keys
+     * @param columns
+     * @param filters
+     * @param object_store_options
+     * @return
+     */
+    public static native LakeSoulArrowReader createLakeSoulArrowReader(String[] files,
+                                                                       String[] primary_keys,
+                                                                       String[] columns,
+                                                                       String[] filters,
+                                                                       HashMap<String, String> object_store_options);
+
+    /**
+     * native method to get ArrowArray memory address
+     * @return
+     */
+    public native long getArrowArrayAddress();
+
+    /**
+     * native method to get ArrowSchema memory address
+     * @return
+     */
+    public native long getArrowSchemaAddress();
+
+    private final BufferAllocator allocator = new RootAllocator();
+    private ArrowSchema arrowSchema = null;
+
+
+
+    public boolean next() {
+        throw new RuntimeException("next() Not implemented");
+    }
+
+    private void importArrowSchema(){
+        long memoryAddress = this.getArrowSchemaAddress();
+        arrowSchema = ArrowSchema.wrap(memoryAddress);
+    }
+
+    private ArrowSchema getArrowSchema(){
+        if (arrowSchema == null) {
+            importArrowSchema();
+        }
+        return arrowSchema;
+    }
+
+    public VectorSchemaRoot getVectorSchemaRootByAddr(){
+
+        long memoryAddress = this.getArrowArrayAddress();
+        ArrowArray importedArrowArray = ArrowArray.wrap(memoryAddress);
+
+        ArrowSchema importedArrowSchema = this.getArrowSchema();
+
+        return Data.importVectorSchemaRoot(allocator, importedArrowArray, importedArrowSchema, null);
+    }
+}
+
