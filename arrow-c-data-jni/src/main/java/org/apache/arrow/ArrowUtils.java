34a054730d48a4c9f4c69ffa8b6fcdc6f849406e
zenghua <441651826@qq.com>
2022-07-21 11:46:38 +0800

    [Arrow][aroow-c-data-jni]add aroow-c-data-jni module

---
+++ arrow-c-data-jni/src/main/java/org/apache/arrow/ArrowUtils.java
@@ -0,0 +1,6 @@
+package org.apache.arrow;
+
+public class ArrowUtils {
+    public final static String LOCAL_BASE_DIR = "/Users/ceng/Documents/GitHub/LakeSoul/arrow-c-data-jni";
+    public final static String LOCAL_ARROW_DIR = LOCAL_BASE_DIR+ "/arrow";
+}
---
+++ arrow-c-data-jni/src/main/java/org/apache/arrow/lakesoul/javacpp/cpp/CDataCppBridge.h
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <arrow/api.h>
+#include <arrow/c/bridge.h>
+
+
+void FillInt64Array(const uintptr_t c_schema_ptr, const uintptr_t c_array_ptr) {
+    arrow::Int64Builder builder;
+    builder.Append(1);
+    builder.Append(2);
+    builder.Append(3);
+    builder.AppendNull();
+    builder.Append(5);
+    builder.Append(6);
+    builder.Append(7);
+    builder.Append(8);
+    builder.Append(9);
+    builder.Append(10);
+    std::shared_ptr<arrow::Array> array = *builder.Finish();
+
+    struct ArrowSchema* c_schema = reinterpret_cast<struct ArrowSchema*>(c_schema_ptr);
+    auto c_schema_status = arrow::ExportType(*array->type(), c_schema);
+//    std::shared_ptr<arrow::Schema> schema =
+//            arrow::schema({arrow::field("x", arrow::int64())});
+//    auto c_schema_status = arrow::ExportSchema(*schema, c_schema);
+
+    if (!c_schema_status.ok()) c_schema_status.Abort();
+
+    struct ArrowArray* c_array = reinterpret_cast<struct ArrowArray*>(c_array_ptr);
+    auto c_array_status = arrow::ExportArray(*array, c_array);
+    if (!c_array_status.ok()) c_array_status.Abort();
+}
+
+void FillInt64Batch(const uintptr_t c_schema_ptr, const uintptr_t c_array_ptr) {
+    arrow::Int64Builder builder;
+    builder.Append(1);
+    builder.Append(2);
+    builder.Append(3);
+    builder.AppendNull();
+    builder.Append(5);
+    builder.Append(6);
+    builder.Append(7);
+    builder.Append(8);
+    builder.Append(9);
+    builder.Append(10);
+    std::shared_ptr<arrow::Array> array = *builder.Finish();
+
+    std::shared_ptr<arrow::Schema> schema = arrow::schema({arrow::field("x", arrow::int64())});
+
+    std::vector<std::shared_ptr<arrow::Array>> arrays_;
+
+    arrays_.push_back(array);
+    std::shared_ptr<arrow::RecordBatch> batch = arrow::RecordBatch::Make(schema, 10, arrays_);
+
+    struct ArrowSchema* c_schema = reinterpret_cast<struct ArrowSchema*>(c_schema_ptr);
+    struct ArrowArray* c_array = reinterpret_cast<struct ArrowArray*>(c_array_ptr);
+
+    auto c_batch_status = arrow::ExportRecordBatch(*batch, c_array, c_schema);
+    if (!c_batch_status.ok()) c_batch_status.Abort();
+
+
+
+}
+
