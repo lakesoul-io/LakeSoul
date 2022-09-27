e6ca83111eba0fbb3b06b8da1547b1dfc21d07ca
dmetasoul01 <opensource@dmetasoul.com>
2022-09-27 16:55:35 +0800

    use service transformer to merge services

--- lakesoul-flink/src/test/resources/flink_sql_test.sql
+++ lakesoul-flink/src/test/resources/flink_sql_test.sql
@@ -2,23 +2,24 @@ SET 'parallelism.default' = '1';
 
 CREATE CATALOG lakesoul_catalog WITH ('type'='lakesoul');
 
-use catalog lakesoul_catalog;
+USE CATALOG lakesoul_catalog;
 
-CREATE TABLE IF NOT EXISTS test_cdc.mysql_test_1_2 ( id int,
-                                       name string,
-                                       dt int,
-                                       primary key (id) NOT ENFORCED )
-    PARTITIONED BY (dt)
-    with ('connector'='lakesoul',
-    'format'='parquet',
-    'path'='/tmp/lakesoul/test_cdc/mysql_test_1_2',
-    'useCDC'='true',
-    'hashBucketNum'='2');
+CREATE TABLE IF NOT EXISTS test_cdc.mysql_test_1_2 (
+    id int,
+    name string,
+    dt int,
+    PRIMARY KEY (id) NOT ENFORCED )
+PARTITIONED BY (dt)
+WITH ('connector'='lakesoul',
+'format'='parquet',
+'path'='/tmp/lakesoul/test_cdc/mysql_test_1_2',
+'useCDC'='true',
+'hashBucketNum'='2');
 
-use catalog default_catalog;
+USE CATALOG default_catalog;
 
-create table mysql_test_1(id INTEGER primary key NOT ENFORCED, name string, dt int)
-    with (
+CREATE TABLE mysql_test_1(id INTEGER PRIMARY KEY NOT ENFORCED, name string, dt int)
+    WITH (
         'connector'='mysql-cdc',
         'hostname'='127.0.0.1',
         'port'='3306',
@@ -28,4 +29,4 @@ create table mysql_test_1(id INTEGER primary key NOT ENFORCED, name string, dt i
         'database-name'='test_cdc',
         'table-name'='mysql_test_1');
 
-insert into lakesoul_catalog.test_cdc.mysql_test_1_2 select * from mysql_test_1;
+INSERT INTO lakesoul_catalog.test_cdc.mysql_test_1_2 SELECT * FROM mysql_test_1;
