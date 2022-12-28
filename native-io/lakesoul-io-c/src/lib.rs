206b5905be65ed0b82bd6aac3e45d961bbfad07b
zenghua <441651826@qq.com>
2022-12-28 17:31:44 +0800

    cleanup code

--- native-io/lakesoul-io-c/src/lib.rs
+++ native-io/lakesoul-io-c/src/lib.rs
@@ -168,7 +168,6 @@ pub extern "C" fn lakesoul_config_builder_set_buffer_size(
 ) -> NonNull<ReaderConfigBuilder> {
     // println!("Setting batch_size={} for lakesoul_config_builder", batch_size);
     convert_to_opaque(from_opaque::<ReaderConfigBuilder, LakeSoulReaderConfigBuilder>(builder))
-    // .with_buffer_size(buffer_size))
 }
 
 #[no_mangle]
