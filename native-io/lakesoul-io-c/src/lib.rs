b1832a386e0f4ac124e33a64cdf17df8c5ff50a2
zenghua <441651826@qq.com>
2022-12-28 11:00:44 +0800

    fix compile error caused by deprecated method

--- native-io/lakesoul-io-c/src/lib.rs
+++ native-io/lakesoul-io-c/src/lib.rs
@@ -167,7 +167,10 @@ pub extern "C" fn lakesoul_config_builder_set_buffer_size(
     buffer_size: c_size_t,
 ) -> NonNull<ReaderConfigBuilder> {
     // println!("Setting batch_size={} for lakesoul_config_builder", batch_size);
-    convert_to_opaque(from_opaque::<ReaderConfigBuilder, LakeSoulReaderConfigBuilder>(builder).with_buffer_size(buffer_size))
+    // convert_to_opaque(from_opaque::<ReaderConfigBuilder, LakeSoulReaderConfigBuilder>(builder).with_buffer_size(buffer_size))
+
+    // method with_buffer_size is deprecated
+    convert_to_opaque(from_opaque::<ReaderConfigBuilder, LakeSoulReaderConfigBuilder>(builder))
 }
 
 #[no_mangle]
