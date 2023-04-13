887b5974acc249f4165b4109d07ec0e8956f5c61
fphantam <f-phantam@outlook.com>
2023-04-13 18:47:49 +0800

    add auto compaction task

---
+++ lakesoul-spark/src/main/scala/com/dmetasoul/lakesoul/spark/compaction/CompactionTask.scala
@@ -0,0 +1,95 @@
+package com.dmetasoul.lakesoul.spark.compaction
+
+import com.dmetasoul.lakesoul.meta.DBConnector
+import com.dmetasoul.lakesoul.tables.LakeSoulTable
+import com.google.gson.{JsonObject, JsonParser}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.lakesoul.catalog.LakeSoulCatalog
+import org.postgresql.PGConnection
+
+import java.util.concurrent.Executors
+
+object CompactionTask {
+
+  val COMPACTION_THREADPOOL_SIZE = 10
+  val NOTIFY_CHANNEL_NAME = "lakesoul_compaction_notify"
+
+  def main(args: Array[String]): Unit = {
+
+    val builder = SparkSession.builder()
+      .appName("COMPACTION TASK")
+      .master("local[4]")
+      .config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+      .config("hadoop.fs.s3a.committer.name", "directory")
+      .config("spark.hadoop.fs.s3a.committer.staging.conflict-mode", "append")
+      .config("spark.hadoop.fs.s3a.committer.staging.tmp.path", "/opt/spark/work-dir/s3a_staging")
+      .config("spark.hadoop.mapreduce.outputcommitter.factory.scheme.s3a", "org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory")
+      .config("spark.hadoop.fs.s3a.path.style.access", "true")
+      .config("spark.hadoop.fs.s3.buffer.dir", "/opt/spark/work-dir/s3")
+      .config("spark.hadoop.fs.s3a.buffer.dir", "/opt/spark/work-dir/s3a")
+      .config("spark.hadoop.fs.s3a.fast.upload.buffer", "disk")
+      .config("spark.hadoop.fs.s3a.fast.upload", value = true)
+      .config("spark.hadoop.fs.s3a.multipart.size", 67108864)
+      .config("spark.hadoop.fs.s3a.connection.maximum", 100)
+      .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+      .config("spark.hadoop.fs.s3a.endpoint.region", "us-east-1")
+      .config("spark.hadoop.fs.s3a.access.key", "minioadmin1")
+      .config("spark.hadoop.fs.s3a.secret.key", "minioadmin1")
+      .config("spark.sql.shuffle.partitions", 10)
+      .config("spark.sql.files.maxPartitionBytes", "1g")
+      .config("spark.default.parallelism", 8)
+      .config("spark.sql.parquet.mergeSchema", value = false)
+      .config("spark.sql.parquet.filterPushdown", value = true)
+      .config("spark.hadoop.mapred.output.committer.class", "org.apache.hadoop.mapred.FileOutputCommitter")
+      .config("spark.sql.warehouse.dir", "s3://lakesoul-test-bucket/")
+      .config("spark.sql.session.timeZone", "Asia/Shanghai")
+      .config("spark.sql.extensions", "com.dmetasoul.lakesoul.sql.LakeSoulSparkSessionExtension")
+      .config("spark.sql.catalog.lakesoul", classOf[LakeSoulCatalog].getName)
+      .config(SQLConf.DEFAULT_CATALOG.key, LakeSoulCatalog.CATALOG_NAME)
+      .config("spark.dmetasoul.lakesoul.native.io.enable", "true")
+
+    val spark = builder.getOrCreate()
+
+    new Listener().start()
+
+  }
+
+  class Listener extends Thread {
+    private val conn = DBConnector.getConn
+    private val pgconn = conn.unwrap(classOf[PGConnection])
+
+    val threadPool = Executors.newFixedThreadPool(COMPACTION_THREADPOOL_SIZE)
+
+    override def run(): Unit = {
+      val stmt = conn.createStatement
+      stmt.execute("LISTEN " + NOTIFY_CHANNEL_NAME)
+      stmt.close()
+
+      val jsonParser = new JsonParser()
+      while (true) {
+        val notifications = pgconn.getNotifications
+        if (notifications.length > 0) {
+          notifications.foreach(notification => {
+            val jsonObj = jsonParser.parse(notification.getParameter).asInstanceOf[JsonObject]
+            println(jsonObj)
+            val tablePath = jsonObj.get("table_path").getAsString
+            val partitionDesc = jsonObj.get("table_partition_desc").getAsString
+            val rsPartitionDesc = partitionDesc.replace("=", "='") + "'"
+            threadPool.execute(new CompactionTableInfo(tablePath, rsPartitionDesc))
+          })
+        }
+        Thread.sleep(10000)
+      }
+    }
+  }
+
+  class CompactionTableInfo(path: String, partitionDesc: String) extends Thread {
+    override def run(): Unit = {
+      println("table_path: " + path)
+      println("table_partition_desc: " + partitionDesc)
+      val table = LakeSoulTable.forPath(path)
+      table.compaction(partitionDesc)
+    }
+  }
+}
