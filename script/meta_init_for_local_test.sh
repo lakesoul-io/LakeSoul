ff704e6e6b817a495a44d51f69a3dcb70256b0a8
chenxu <chenxu@dmetasoul.com>
2023-06-02 10:49:55 +0800

    use profile for parallel test
    Signed-off-by: chenxu <chenxu@dmetasoul.com>

--- script/meta_init_for_local_test.sh
+++ script/meta_init_for_local_test.sh
@@ -6,11 +6,36 @@ set -ex
 
 BASEDIR=$(dirname "$0")
 
-docker run -d --name lakesoul-test-pg -p5432:5432 -e POSTGRES_USER=lakesoul_test -e POSTGRES_PASSWORD=lakesoul_test -e POSTGRES_DB=lakesoul_test -d postgres:14.5
-sleep 30s
+PARALLELISM=1
 
-PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -f "$BASEDIR"/meta_init.sql lakesoul_test
-PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -c "CREATE DATABASE lakesoul_test_1"
-PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -f "$BASEDIR"/meta_init.sql lakesoul_test_1
-PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -c "CREATE DATABASE lakesoul_test_2"
-PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -f "$BASEDIR"/meta_init.sql lakesoul_test_2
+usage() {
+  echo "Usage: $0 -j NUM_PARALLEL_FOR_TEST"
+}
+
+while getopts "hj:" arg; do
+  case $arg in
+    j)
+      PARALLELISM=${OPTARG}
+      echo "Create $PARALLELISM database for testing"
+      ;;
+    h | *)
+      usage
+      exit 0
+      ;;
+  esac
+done
+
+init_database() {
+  PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -tc "SELECT 1 FROM pg_database WHERE datname = '$1'" | grep -q 1 || PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -c "CREATE DATABASE $1"
+  PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -f "$BASEDIR"/meta_init.sql $1
+  PGPASSWORD=lakesoul_test psql -h localhost -p 5432 -U lakesoul_test -f "$BASEDIR"/meta_cleanup.sql $1
+}
+
+init_database "lakesoul_test"
+
+if [ "$PARALLELISM" -gt "1" ]; then
+  for i in $(seq 1 "$PARALLELISM");
+  do
+    init_database "lakesoul_test_$i"
+  done
+fi
