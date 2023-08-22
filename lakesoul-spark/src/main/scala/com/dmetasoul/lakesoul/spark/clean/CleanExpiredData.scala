f18ba304b67268fb2c35a14c9f5edd3379f17360
ChenYunHey <1908166778@qq.com>
2023-08-22 16:48:13 +0800

    add file head for new files
    Signed-off-by: ChenYunHey <1908166778@qq.com>

--- lakesoul-spark/src/main/scala/com/dmetasoul/lakesoul/spark/clean/CleanExpiredData.scala
+++ lakesoul-spark/src/main/scala/com/dmetasoul/lakesoul/spark/clean/CleanExpiredData.scala
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 LakeSoul Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package com.dmetasoul.lakesoul.spark.clean
 
 import com.dmetasoul.lakesoul.meta.DBConnector
