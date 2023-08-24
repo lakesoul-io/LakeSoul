352aeccad18e30f50e062abdc6b71019ad96429d
Xiong HuaiDong <xionghd@dmetasoul.com>
2023-08-24 17:15:05 +0800

    Build and install shared libraries into python directory.
    Signed-off-by: Xiong HuaiDong <xionghd@dmetasoul.com>

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -6,14 +6,15 @@
 
 if [ -f $(dirname ${BASH_SOURCE[0]})/build/build.ninja ]
 then
-    cmake --build $(dirname ${BASH_SOURCE[0]})/build --config Release
+    cmake --build $(dirname ${BASH_SOURCE[0]})/build --config Release --target install
     exit
 fi
 
 set -e
 
 rm -rf $(dirname ${BASH_SOURCE[0]})/build
-cmake -G Ninja -S $(dirname ${BASH_SOURCE[0]}) \
-      -B $(dirname ${BASH_SOURCE[0]})/build    \
-      -DCMAKE_BUILD_TYPE=Release
-cmake --build $(dirname ${BASH_SOURCE[0]})/build --config Release
+cmake -G Ninja -S $(dirname ${BASH_SOURCE[0]})                               \
+      -B $(dirname ${BASH_SOURCE[0]})/build                                  \
+      -DCMAKE_BUILD_TYPE=Release                                             \
+      -DCMAKE_INSTALL_PREFIX=$(dirname ${BASH_SOURCE[0]})/../python/lakesoul
+cmake --build $(dirname ${BASH_SOURCE[0]})/build --config Release --target install
