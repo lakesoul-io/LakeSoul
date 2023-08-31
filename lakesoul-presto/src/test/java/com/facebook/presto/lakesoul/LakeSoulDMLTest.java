33990759586fe3657eab142ab53477bf121b0524
yuanf <yuanf@dmetasoul.com>
2023-08-31 17:26:01 +0800

    test ok with tables generated by flink
    Signed-off-by: yuanf <yuanf@dmetasoul.com>

--- lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulDMLTest.java
+++ lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulDMLTest.java
@@ -20,77 +20,48 @@ public class LakeSoulDMLTest extends LakeSoulSmokeTest{
         assert rows2.size() == 5;
     }
 
-    /* without pks */
-    public void testPrimaryKey1(){
-        List<MaterializedRow> rows = sql("select name, id from table1 where id >= 2");
-        System.out.println(rows);
-    }
-
     /* defined by pks, and projected without pks */
     public void testPrimaryKey2(){
-        List<MaterializedRow> rows = sql("select math from table4 where id > 2");
+        List<MaterializedRow> rows = sql("select col1, col2 from pks.table1 where col3 > 2");
         System.out.println(rows);
-        assert rows.size() == 1 && rows.get(0).getField(0).equals(95.2);
+        assert rows.size() == 3;
     }
 
 
     /* defined by pks, and projected with pks */
     public void testPrimaryKey3(){
-        List<MaterializedRow> rows = sql("select name, id from table4 where math > 97");
+        List<MaterializedRow> rows = sql("select id1, id2 from pks.table1 where col3 > 2");
         System.out.println(rows);
         assert rows.size() == 3;
     }
 
     /* defined by pks, and projected with part of pks */
     public void testPrimaryKey4(){
-        List<MaterializedRow> rows = sql("select id, math from table4 where name = 'xiao li'");
+        List<MaterializedRow> rows = sql("select id1, col2 from pks.table1 where col3 > 2");
         System.out.println(rows);
-        assert rows.size() == 2;
+        assert rows.size() == 3;
     }
 
-    /* all */
-    public void testPartition0(){
-        List<MaterializedRow> rows = sql("select * from table2 ");
-        System.out.println(rows);
-        assert rows.size() == 4;
-    }
 
     /* defined by rks, and projected without rks */
     public void testPartition1(){
-        List<MaterializedRow> rows = sql("select math from table2 where id > 2");
+        List<MaterializedRow> rows = sql("select col1, col2 from rks.table1 where col3 > 2");
         System.out.println(rows);
-        assert rows.size() == 1 && rows.get(0).getField(0).equals(95.2);
+        assert rows.size() == 3;
     }
 
     /* defined by rks, and projected with rks */
     public void testPartition2(){
-        List<MaterializedRow> rows = sql("select name, id from table4 where math > 97");
+        List<MaterializedRow> rows = sql("select id1, id2 from rks.table1 where col3 > 2");
         System.out.println(rows);
         assert rows.size() == 3;
     }
 
     /* defined by rks, and projected with part of rks */
     public void testPartition3(){
-        List<MaterializedRow> rows = sql("select id, math from table2 where name = 'xiaoli'");
+        List<MaterializedRow> rows = sql("select id1, col2 from rks.table1 where col3 > 2");
         System.out.println(rows);
-        assert rows.size() == 2;
-    }
-
-    public void testWhere1(){
-        // coming soon
-
-        List<MaterializedRow> rows = sql("select * from test_cdc.table2");
-        System.out.println(rows);
-        assert rows.size() == 5;
-    }
-
-
-    public void testGroupBy1(){
-        // coming soon
-    }
-
-    public void testJoin1(){
-        // coming soon
+        assert rows.size() == 3;
     }
 
 
--- lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulTypeTest.java
+++ lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulTypeTest.java
@@ -6,91 +6,134 @@ package com.facebook.presto.lakesoul;
 
 import com.facebook.presto.testing.MaterializedRow;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LakeSoulTypeTest extends LakeSoulSmokeTest {
 
     public void testTinyIntType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id1 from table3 where id1 > 1");
+        List<MaterializedRow> showColumnsInTable1 = sql("select col1 from pks.table1 where col1 > 1");
+        System.out.println(showColumnsInTable1);
         assert showColumnsInTable1.size() == 4;
     }
 
     public void testShortType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id2 from table3 where id2 > 2");
-        assert showColumnsInTable1.size() == 3;
+        List<MaterializedRow> showColumnsInTable1 = sql("select col2 from pks.table1 where col2 > 1");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 4;
     }
 
     public void testIntType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id3 from table3 where id3 > 3");
-        assert showColumnsInTable1.size() == 2;
+        List<MaterializedRow> showColumnsInTable1 = sql("select col3 from pks.table1 where col3 > 1");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 4;
     }
 
     public void testLongType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id4 from table3 where id4 > 4");
+        List<MaterializedRow> showColumnsInTable1 = sql("select col4 from pks.table1 where col4 > 4");
+        System.out.println(showColumnsInTable1);
         assert showColumnsInTable1.size() == 1;
     }
 
     public void testFloatType(){
-        //List<MaterializedRow> showColumnsInTable1 = sql("select id5 from table3 where id5 >= 5.0");
-        List<MaterializedRow> showColumnsInTable1 = sql("select id5 from table3 where id5 >= 5.0");
-        List<MaterializedRow> showColumnsInTable2 = sql("select id5 from table3 where id5 >= 5");
-        List<MaterializedRow> showColumnsInTable3 = sql("select id5 from table3 where id5 = 5");
-        List<MaterializedRow> showColumnsInTable4 = sql("select id5 from table3 where id5 = 5.0");
+        List<MaterializedRow> showColumnsInTable1 = sql("select col5 from pks.table1 where col5 >= 5.0");
+        List<MaterializedRow> showColumnsInTable2 = sql("select col5 from pks.table1 where col5 >= 5");
+        List<MaterializedRow> showColumnsInTable3 = sql("select col5 from pks.table1 where col5 = 5.11");
         System.out.println(showColumnsInTable1);
         System.out.println(showColumnsInTable2);
         System.out.println(showColumnsInTable3);
-        System.out.println(showColumnsInTable4);
-        assert showColumnsInTable1.size() == 1 && showColumnsInTable1.get(0).getField(0).equals(5.0f);
-        assert showColumnsInTable2.size() == 1 && showColumnsInTable2.get(0).getField(0).equals(5.0f);
-        assert showColumnsInTable3.size() == 1 && showColumnsInTable3.get(0).getField(0).equals(5.0f);
-        assert showColumnsInTable4.size() == 1 && showColumnsInTable4.get(0).getField(0).equals(5.0f);
+        assert showColumnsInTable1.size() == 1 && showColumnsInTable1.get(0).getField(0).equals(5.11f);
+        assert showColumnsInTable2.size() == 1 && showColumnsInTable2.get(0).getField(0).equals(5.11f);
+        assert showColumnsInTable3.size() == 1 && showColumnsInTable3.get(0).getField(0).equals(5.11f);
     }
 
     public void testDoubleType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id6 from table3 where id6 >= 5");
-        assert showColumnsInTable1.size() == 1;
+        List<MaterializedRow> showColumnsInTable1 = sql("select col6 from pks.table1 where col6 >= 5.0");
+        List<MaterializedRow> showColumnsInTable2 = sql("select col6 from pks.table1 where col6 >= 5");
+        List<MaterializedRow> showColumnsInTable3 = sql("select col6 from pks.table1 where col6 = 5.111");
+        System.out.println(showColumnsInTable1);
+        System.out.println(showColumnsInTable2);
+        System.out.println(showColumnsInTable3);
+        assert showColumnsInTable1.size() == 1 && showColumnsInTable1.get(0).getField(0).equals(5.111);
+        assert showColumnsInTable2.size() == 1 && showColumnsInTable2.get(0).getField(0).equals(5.111);
+        assert showColumnsInTable3.size() == 1 && showColumnsInTable3.get(0).getField(0).equals(5.111);
     }
 
     public void testBooleanType(){
         //List<MaterializedRow> showColumnsInTable1 = sql("select * from table4");
-        List<MaterializedRow> showColumnsInTable1 = sql("select id1 from table4 where id1 = true");
-        assert showColumnsInTable1.size() == 3;
+        List<MaterializedRow> showColumnsInTable1 = sql("select id1 from pks.table1 where col7 = true");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 2 ;
+        Set<Object> collect = showColumnsInTable1.stream().map((item) -> item.getField(0)).collect(Collectors.toSet());
+        collect.contains(1);
+        collect.contains(2);
     }
 
     public void testDecimalType(){
-        // 上层过了过滤
-        List<MaterializedRow> showColumnsInTable1 = sql("select id2 from table4 where id2 > 2 ");
+        // filted upon upper layer
+        List<MaterializedRow> showColumnsInTable1 = sql("select col8 from pks.table1 where col8 > 2 ");
+        List<MaterializedRow> showColumnsInTable2 = sql("select col9 from pks.table1 where col9 > 2.0 ");
+        System.out.println(showColumnsInTable1);
+        System.out.println(showColumnsInTable2);
         assert showColumnsInTable1.size() == 4;
+        assert showColumnsInTable2.size() == 4;
+    }
+
+    public void testCharsType(){
+        List<MaterializedRow> showColumnsInTable1 = sql("select col10 from pks.table1 where col10 = 'a'");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 1 && showColumnsInTable1.get(0).getField(0).equals("a");
     }
 
     public void testStringType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select * from table4 where id3 = '1.111'");
-        //List<MaterializedRow> showColumnsInTable1 = sql("select id3 from table4 where id3 = 'hello' or id3 is null");
-        assert showColumnsInTable1.size() == 1;
+        List<MaterializedRow> showColumnsInTable1 = sql("select col11 from pks.table1  where col11 = 'a'");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 1 && showColumnsInTable1.get(0).getField(0).equals("a");
     }
 
     public void testByteType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id1 from table5 where id1 = 127");
-        assert showColumnsInTable1.size() == 1;
-    }
-
-    public void testDateType(){
-        List<MaterializedRow> showColumnsInTable1 = sql("select id2 from table5 where id2 = date('2023-8-30') ");
-        assert showColumnsInTable1.size() == 1;
-    }
-
-    public void testTimeStamp(){
-//        List<MaterializedRow> showColumnsInTable1 = sql("select id3 from table5 where id3 > timestamp '2023-08-29 17:01:02' ");
-//        assert showColumnsInTable1.size() == 1;
-    }
-
-    public void testTimeStampWithTimeZone(){
-//        List<MaterializedRow> showColumnsInTable1 = sql("select id3 from table5 where id3 > timestamp '2023-08-29 17:01:02' ");
-//        assert showColumnsInTable1.size() == 1;
+        // coming soon
     }
 
     public void testBinary(){
+        List<MaterializedRow> showColumnsInTable1 = sql("select col12 from pks.table1  where col11 = 'b'");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 1 && bytesEq((byte[]) showColumnsInTable1.get(0).getField(0), "bbb".getBytes());
+    }
+
+    public void testVarBinary(){
+        List<MaterializedRow> showColumnsInTable1 = sql("select col13 from pks.table1  where col11 = 'b'");
+        System.out.println(showColumnsInTable1);
+        assert showColumnsInTable1.size() == 1 && bytesEq((byte[]) showColumnsInTable1.get(0).getField(0), "bbbb".getBytes());
+    }
+
+    public void testDateType(){
+        List<MaterializedRow> showColumnsInTable1 = sql("select col14 from pks.table1 where col14 = date('2023-09-03') ");
+        assert showColumnsInTable1.size() == 1;
+        System.out.println(showColumnsInTable1);
+        LocalDate localDate = (LocalDate) showColumnsInTable1.get(0).getField(0);
+        assert localDate.equals(LocalDate.of(2023, 9, 3));
+    }
+
+
+
+    public void testTimeStamp(){
+        // coming soon
 
     }
 
+    public void testTimeStampWithTimeZone(){
+        // coming soon
+    }
+
+
+    private boolean bytesEq(byte[] a, byte[] b){
+        if(a.length != b.length) return false;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
 }
