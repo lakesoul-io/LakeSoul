5db08f337dec1dabd585fa26bf922e0e213dc268
yuanf <yuanf@dmetasoul.com>
2023-09-01 10:56:11 +0800

    add more tip info
    Signed-off-by: yuanf <yuanf@dmetasoul.com>

--- lakesoul-presto/src/test/java/com/facebook/presto/benchmark/Benchmark.java
+++ lakesoul-presto/src/test/java/com/facebook/presto/benchmark/Benchmark.java
@@ -138,6 +138,10 @@ public class Benchmark {
             int count3 = getCount(prestoCon.prepareStatement(sql3).executeQuery());
             int count4 = getCount(prestoCon.prepareStatement(sql4).executeQuery());
             if(count1 == 0 || count2 == 0 || count3 != 0 || count4 != 0){
+                System.out.println("count1=" + count1);
+                System.out.println("count1=" + count2);
+                System.out.println("count1=" + count3);
+                System.out.println("count1=" + count4);
                 throw new RuntimeException("table " + table + " is not matched");
             }
             System.out.println("table " + table + " matched");
