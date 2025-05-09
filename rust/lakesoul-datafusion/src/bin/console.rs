9db52325fb373b09447b072ad00d474293a2a88d
mag1c1an1 <mag1cian@icloud.com>
2025-05-09 19:55:42 +0800

    fix ci
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- rust/lakesoul-datafusion/src/bin/console.rs
+++ rust/lakesoul-datafusion/src/bin/console.rs
@@ -9,6 +9,7 @@ use clap::Parser;
 use lakesoul_datafusion::{cli::CoreArgs, create_lakesoul_session_ctx};
 use lakesoul_metadata::MetaDataClient;
 use rustyline::DefaultEditor;
+use rustyline::error::ReadlineError;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() {
@@ -21,44 +22,28 @@ async fn main() {
 
     let ctx = create_lakesoul_session_ctx(meta_client, &core_args).unwrap();
 
-    // let sql = "
-    //    CREATE EXTERNAL TABLE
-    //         IF NOT EXISTS lakesoul.default.all_types_lfs
-    //         (c1 VARCHAR NOT NULL, c2 INT NOT NULL, c3 DOUBLE)
-    //         STORED AS LAKESOUL
-    //         LOCATION 'file:///tmp/LAKESOUL/test_lfs_data'
-// ";
+    loop {
+        let readline = rl.readline("lakesoul >> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).unwrap();
 
-    // ctx.sql(sql).await.unwrap().show().await.unwrap();
-    ctx.sql("select * from lakesoul.default.all_type_lfs")
-        .await
-        .unwrap()
-        .show()
-        .await
-        .unwrap();
-
-    // loop {
-    //     let readline = rl.readline("lakesoul >> ");
-    //     match readline {
-    //         Ok(line) => {
-    //             rl.add_history_entry(line.as_str()).unwrap();
-
-    //             match ctx.sql(line.as_str()).await {
-    //                 Ok(df) => df.show().await.unwrap(),
-    //                 Err(e) => println!("Error: {:?}", e),
-    //             }
-    //         }
-    //         Err(ReadlineError::Interrupted) => {
-    //             break;
-    //         }
-    //         Err(ReadlineError::Eof) => {
-    //             println!("bye bye! 🫡");
-    //             break;
-    //         }
-    //         Err(err) => {
-    //             println!("Error: {:?}", err);
-    //             break;
-    //         }
-    //     }
-    // }
+                match ctx.sql(line.as_str()).await {
+                    Ok(df) => df.show().await.unwrap(),
+                    Err(e) => println!("Error: {:?}", e),
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("bye bye! 🫡");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
