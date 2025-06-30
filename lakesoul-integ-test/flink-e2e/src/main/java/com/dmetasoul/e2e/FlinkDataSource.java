f4a7bc31ee6cd7e9f4e26b846fd0fd51f92e5110
mag1c1an1 <mag1cian@icloud.com>
2025-06-30 19:05:03 +0800

    e2e: use parquet
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- lakesoul-integ-test/flink-e2e/src/main/java/com/dmetasoul/e2e/FlinkDataSource.java
+++ lakesoul-integ-test/flink-e2e/src/main/java/com/dmetasoul/e2e/FlinkDataSource.java
@@ -57,7 +57,7 @@ public class FlinkDataSource {
             "f_row ROW<f1 INT, f2 STRING>"
             + ") WITH (\n"
             + "'connector' = 'filesystem',\n"
-            + "'path' = 'file:///tmp/lakesoul/e2e/data/',\n"
+            + "'path' = 's3://lakesoul-test-bucket/lakesoul/e2e/data/',\n"
             + "'format' ='parquet'\n"
             + ")\n";
     tableEnv.executeSql(parquetFileTable).await();
