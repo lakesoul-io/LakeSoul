8fd634c6c4b1395f93ba3cd3972326d027779969
Xu Chen <xuchen-plus@users.noreply.github.com>
2025-07-15 15:16:07 +0800

    [Presto] Impl lakesoul functions (#630)
    * [Presto] Impl lakesoul functions
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    
    * fix presto container config
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    
    ---------
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    Co-authored-by: chenxu <chenxu@dmetasoul.com>

--- lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulSmokeTest.java
+++ lakesoul-presto/src/test/java/com/facebook/presto/lakesoul/LakeSoulSmokeTest.java
@@ -33,7 +33,6 @@ public class LakeSoulSmokeTest extends AbstractTestIntegrationSmokeTest {
         return createLakeSoulQueryRunner();
     }
 
-    @BeforeClass
     public void setUp()
     {
         lakeSoulQueryRunner = (LakeSoulQueryRunner) getQueryRunner();
