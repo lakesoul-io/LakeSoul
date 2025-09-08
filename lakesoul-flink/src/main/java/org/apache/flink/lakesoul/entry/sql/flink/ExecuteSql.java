f26ef2210275391de06f750ab933eb8122c602b4
ChenYunHey <1908166778@qq.com>
2025-09-08 18:07:31 +0800

    ..
    Signed-off-by: ChenYunHey <1908166778@qq.com>

--- lakesoul-flink/src/main/java/org/apache/flink/lakesoul/entry/sql/flink/ExecuteSql.java
+++ lakesoul-flink/src/main/java/org/apache/flink/lakesoul/entry/sql/flink/ExecuteSql.java
@@ -92,7 +92,7 @@ public class ExecuteSql {
 
             // try get k8s cluster name
             String k8sClusterID = conf.getString("kubernetes.cluster-id", "");
-            env.execute(k8sClusterID.isEmpty() ? null : k8sClusterID + "-job");
+            env.execute(k8sClusterID.isEmpty() ? null : k8sClusterID);
         } else {
             System.out.println("There's no INSERT INTO statement, the program will terminate");
         }
