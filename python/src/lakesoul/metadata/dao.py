c9003fcdbc547e4327471f1e94308908ec4d159d
mag1c1an1 <mag1cian@icloud.com>
2025-09-11 06:27:23 +0000

    add metadata stub
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- python/src/lakesoul/metadata/dao.py
+++ python/src/lakesoul/metadata/dao.py
@@ -2,38 +2,52 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import Optional, Sequence
 from .lib.const import DaoType
 from .native_client import query
 
-
-def select_table_info_by_table_name(table_name, namespace="default"):
-    wrapper = query(DaoType.SelectTableInfoByTableNameAndNameSpace, [table_name, namespace])
-    if wrapper is None:
-        message = "table %r is not found in namespace %r" % (table_name, namespace)
-        raise RuntimeError(message)
-    return wrapper.table_info[0]
+from .generated import entity_pb2
 
 
-def get_partition_info_by_table_id(table_id):
+def select_table_info_by_table_name(
+    table_name, namespace="default"
+) -> entity_pb2.TableInfo:
+    wrapper = query(
+        DaoType.SelectTableInfoByTableNameAndNameSpace, [table_name, namespace]
+    )
+    if wrapper:
+        return wrapper.table_info[0]
+    raise RuntimeError(f"table {table_name} is not found in namseapce {namespace}")
+
+
+def get_partition_info_by_table_id(
+    table_id,
+) -> Optional[Sequence[entity_pb2.PartitionInfo]]:
     wrapper = query(DaoType.ListPartitionByTableId, [table_id])
-    if wrapper is None:
-        return None
-    return wrapper.partition_info
+    if wrapper:
+        return wrapper.partition_info
+    return None
 
 
-def get_partition_info_by_table_id_and_desc(table_id, desc):
+def get_partition_info_by_table_id_and_desc(
+    table_id, desc
+) -> Optional[Sequence[entity_pb2.PartitionInfo]]:
     wrapper = query(DaoType.ListPartitionByTableIdAndDesc, [table_id, desc])
-    if wrapper is None:
-        return None
-    return wrapper.partition_info
+    if wrapper:
+        return wrapper.partition_info
+    return None
 
 
-def list_data_commit_info(table_id, partition_desc, commit_id_list):
+def list_data_commit_info(
+    table_id, partition_desc, commit_id_list
+) -> Optional[Sequence[entity_pb2.DataCommitInfo]]:
     joined_commit_id = ""
     for commit_id in commit_id_list:
         joined_commit_id += "{:016x}{:016x}".format(commit_id.high, commit_id.low)
-    wrapper = query(DaoType.ListDataCommitInfoByTableIdAndPartitionDescAndCommitList,
-                    [table_id, partition_desc, joined_commit_id])
-    if wrapper is None:
-        return None
-    return wrapper.data_commit_info
+    wrapper = query(
+        DaoType.ListDataCommitInfoByTableIdAndPartitionDescAndCommitList,
+        [table_id, partition_desc, joined_commit_id],
+    )
+    if wrapper:
+        return wrapper.data_commit_info
+    return None
