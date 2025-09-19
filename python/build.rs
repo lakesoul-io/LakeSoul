19d6f5e0437e2bb676758795e16d471ff9a545d2
mag1c1an1 <mag1cian@icloud.com>
2025-09-19 01:48:22 +0000

    add sys python
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- python/build.rs
+++ python/build.rs
@@ -1,11 +1,22 @@
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
 fn main() {
+    // first time for uv
+    if let Err(_) = gen_py() {
+        Command::new("python3")
+            .args(["-m", "venv", ".venv"])
+            .status()
+            .unwrap();
+        gen_py().unwrap();
+    }
+}
+
+fn gen_py() -> std::io::Result<ExitStatus> {
     let vpy = ".venv/bin/python3";
     let proto_dir = "rust/proto/src";
     let proto_path = "rust/proto/src/entity.proto";
     let out = "python/src/lakesoul/metadata/generated";
-    // ignore error
+
     Command::new(vpy)
         .args([
             "-m",
@@ -16,5 +27,4 @@ fn main() {
             proto_path,
         ])
         .status()
-        .unwrap();
 }
