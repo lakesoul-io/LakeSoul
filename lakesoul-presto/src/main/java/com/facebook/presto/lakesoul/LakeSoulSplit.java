2017553c1de17458b6ca9bc68612532b09124433
baiqiubai <2569722395@qq.com>
2025-12-24 08:48:53 +0000

    [Presto] Fix split allocation logic in Presto
    Signed-off-by: baiqiubai <2569722395@qq.com>

--- lakesoul-presto/src/main/java/com/facebook/presto/lakesoul/LakeSoulSplit.java
+++ lakesoul-presto/src/main/java/com/facebook/presto/lakesoul/LakeSoulSplit.java
@@ -4,6 +4,7 @@
 
 package com.facebook.presto.lakesoul;
 
+import com.facebook.airlift.log.Logger;
 import com.facebook.presto.lakesoul.handle.LakeSoulTableLayoutHandle;
 import com.facebook.presto.lakesoul.pojo.Path;
 import com.facebook.presto.spi.ConnectorSplit;
@@ -13,6 +14,8 @@ import com.facebook.presto.spi.SplitWeight;
 import com.facebook.presto.spi.schedule.NodeSelectionStrategy;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.dmetasoul.lakesoul.meta.BucketingUtils;
+import scala.Option;
 
 import java.util.*;
 
@@ -21,6 +24,8 @@ import static java.util.Objects.requireNonNull;
 public class LakeSoulSplit implements ConnectorSplit {
     private final LakeSoulTableLayoutHandle layout;
     private final List<Path> paths;
+
+    private static final Logger log = Logger.get(LakeSoulSplit.class);
 
     @JsonCreator
     public LakeSoulSplit(
@@ -48,7 +53,27 @@ public class LakeSoulSplit implements ConnectorSplit {
 
     @Override
     public List<HostAddress> getPreferredNodes(NodeProvider nodeProvider) {
-        return nodeProvider.get(paths.get(0).toString());
+	   return nodeProvider.get(buildStableSplitIdentifier());
+    }
+
+    private String buildStableSplitIdentifier() {
+	    
+        String tableName = layout.getTableHandle().getNames().toString();
+        String partitionKey = "";
+        List<String> partitions = layout.getRangeKeys();
+        if (partitions != null && !partitions.isEmpty()) {
+            partitionKey = String.join(",", partitions);
+        }
+        Option<Object> hashBucketId = BucketingUtils.getBucketId(paths.get(0).toString());
+        String fileIdentifier;
+        if (!hashBucketId.isEmpty()) {
+       	    fileIdentifier = "bucket=" + hashBucketId.get();
+        }else {
+            fileIdentifier = "bucket=0";
+        }
+        String result = String.format("LakeSoul:%s#%s#%s", tableName, partitionKey.isEmpty() ? "NO_PARTITION" : partitionKey, fileIdentifier);
+        log.info("buildStableSplitIdentifier partitionKey %s, hashBucketId %s, result %s", partitionKey, fileIdentifier, result);
+        return result;
     }
 
     @Override
