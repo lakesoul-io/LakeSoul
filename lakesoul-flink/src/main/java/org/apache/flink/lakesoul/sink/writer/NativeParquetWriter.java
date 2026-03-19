c9c840e8c563014e50bc36482843c3620edca5f8
mag1c1an1 <103239869+mag1c1an1@users.noreply.github.com>
2026-03-19 16:03:58 +0800

    [FLINK]: fix wrong stable sort option (#743)
    Change-Id: Id29699e5b63921cd08057590d9286fa96a6a6964
    
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- lakesoul-flink/src/main/java/org/apache/flink/lakesoul/sink/writer/NativeParquetWriter.java
+++ lakesoul-flink/src/main/java/org/apache/flink/lakesoul/sink/writer/NativeParquetWriter.java
@@ -92,9 +92,6 @@ public class NativeParquetWriter implements InProgressFileWriter<RowData, String
         Schema arrowSchema = ArrowUtils.toArrowSchema(rowType);
         nativeWriter = new NativeIOWriter(arrowSchema);
         nativeWriter.setPrimaryKeys(primaryKeys);
-        if (!primaryKeys.isEmpty()) {
-            nativeWriter.setOption(STABLE_SORT, "true");
-        }
 
         if (conf.getBoolean(LakeSoulSinkOptions.isMultiTableSource)) {
             nativeWriter.setAuxSortColumns(Collections.singletonList(SORT_FIELD));
@@ -113,6 +110,9 @@ public class NativeParquetWriter implements InProgressFileWriter<RowData, String
         }
 
         FlinkUtil.setIOConfigs(conf, nativeWriter);
+        if (!primaryKeys.isEmpty()) {
+            nativeWriter.setOption(STABLE_SORT, "true");
+        }
         nativeWriter.initializeWriter();
         LOG.info("Initialized NativeParquetWriter: {}", this);
     }
