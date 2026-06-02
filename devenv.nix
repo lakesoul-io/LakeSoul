8e9d7923db070adba5f5cc959d68c2dd0f9206ae
mag1cian <mag1cian@icloud.com>
2026-06-02 18:33:00 +0800

    use devenv
    Signed-off-by: mag1cian <mag1cian@icloud.com>

---
+++ devenv.nix
@@ -0,0 +1,76 @@
+{
+  pkgs,
+  # lib,
+  # config,
+  # inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    clang
+    llvmPackages.libclang
+  ];
+  env = {
+    CC = "${pkgs.clang}/bin/clang";
+    CXX = "${pkgs.clang}/bin/clang++";
+    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+  };
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages.rust = {
+    enable = true;
+    toolchainFile = ./rust-toolchain.toml;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  services.rustfs = {
+    enable = true;
+  };
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    initialDatabases = [{ name = "lakesoul-test"; user = "lakesoul-test"; pass="lakesoul-test";
+      schema = toString ./script/meta_init.sql;
+    }];
+    extensions = extensions: [
+          # extensions .postgis
+          # extensions .timescaledb
+    ];
+    # initialScript = "";
+    # settings .shared_preload_libraries = "timescaledb";
+ };
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    echo dev env
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
