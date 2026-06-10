5a2cc506bd5c6a18648011110f4709e0cc95377f
baiqiubai <liukai@dmetasoul.com>
2026-06-10 09:56:01 +0800

    [Python] Adapt Daft version for Ray compatibility (#777)
    * [Python] Adapt Daft version for Ray compatibility
    
    Signed-off-by: baiqiubai <2569722395@qq.com>
    
    * fix test
    
    Signed-off-by: baiqiubai <2569722395@qq.com>
    
    * fix ci
    
    Signed-off-by: baiqiubai <2569722395@qq.com>
    
    ---------
    
    Signed-off-by: baiqiubai <2569722395@qq.com>

--- python/src/lakesoul/ray/read_lakesoul.py
+++ python/src/lakesoul/ray/read_lakesoul.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Mapping, Optional, 
 import ray
 from ray.data import Dataset
 from ray.data.block import BlockMetadata
-from ray.data.datasource.datasource import Datasource, Reader, ReadTask
+from ray.data.datasource.datasource import Datasource, ReadTask
 
 from ..metadata.meta_ops import (
     get_arrow_schema_by_table_name,
@@ -18,6 +18,24 @@ from ..metadata.meta_ops import (
 
 if TYPE_CHECKING:
     import pyarrow as pa
+
+
+def _cache_dataset_schema(ds: Dataset, arrow_schema: "pa.Schema") -> None:
+    plan = getattr(ds, "_plan", None)
+    if plan is None:
+        return
+
+    cache_schema = getattr(plan, "cache_schema", None)
+    if cache_schema is not None:
+        cache_schema(arrow_schema)
+        return
+
+    cache = getattr(plan, "_cache", None)
+    logical_plan = getattr(ds, "_logical_plan", None)
+    dag = getattr(logical_plan, "dag", None)
+    set_schema = getattr(cache, "set_schema", None)
+    if set_schema is not None and dag is not None:
+        set_schema(dag, arrow_schema)
 
 
 def _read_lakesoul_data_file(
@@ -60,7 +78,7 @@ def _read_empty_lakesoul_table(arrow_schema: "pa.Schema") -> Iterator["pa.Table"
     yield pa.Table.from_batches([], schema=arrow_schema)
 
 
-class _LakeSoulDatasourceReader(Reader):
+class LakeSoulDatasource(Datasource):
     def __init__(
         self,
         table_name: str,
@@ -69,6 +87,7 @@ class _LakeSoulDatasourceReader(Reader):
         partitions: Optional[Dict[str, str]] = None,
         retain_partition_columns: bool = False,
         namespace: str = "default",
+        object_store_configs: Optional[Dict[str, str]] = None,
     ) -> None:
         self._table_name = table_name
         self._batch_size = batch_size
@@ -76,12 +95,13 @@ class _LakeSoulDatasourceReader(Reader):
         self._partitions = partitions
         self._retain_partition_columns = retain_partition_columns
         self._namespace = namespace
+        self._object_store_configs = object_store_configs or {}
 
     def estimate_inmemory_data_size(self) -> Optional[int]:
         return None
 
     def _get_table_metadata(self) -> Tuple[List[str], "pa.Schema"]:
-        data_files, pk_cols = get_data_files_and_pks_by_table_name(
+        data_files, _ = get_data_files_and_pks_by_table_name(
             table_name=self._table_name,
             partitions=self._partitions or {},
             namespace=self._namespace,
@@ -91,18 +111,20 @@ class _LakeSoulDatasourceReader(Reader):
             namespace=self._namespace,
             retain_partition_columns=self._retain_partition_columns,
         )
-        table_metadata = data_files, arrow_schema
-        return table_metadata
+        return data_files, arrow_schema
 
-    def get_read_tasks(self, parallelism: int) -> List[ReadTask]:
+    def get_read_tasks(
+        self,
+        parallelism: int,
+        per_task_row_limit: Optional[int] = None,
+        data_context: Optional[Any] = None,
+    ) -> List[ReadTask]:
         # useless parallelism parameter
         read_tasks: List[ReadTask] = []
-        table_metadata = self._get_table_metadata()
-        data_files, arrow_schema = table_metadata
+        data_files, arrow_schema = self._get_table_metadata()
         metadata = BlockMetadata(
             num_rows=None,
             size_bytes=None,
-            schema=arrow_schema,
             input_files=None,
             exec_stats=None,
         )
@@ -110,7 +132,6 @@ class _LakeSoulDatasourceReader(Reader):
             empty_metadata = BlockMetadata(
                 num_rows=0,
                 size_bytes=0,
-                schema=arrow_schema,
                 input_files=[],
                 exec_stats=None,
             )
@@ -120,6 +141,8 @@ class _LakeSoulDatasourceReader(Reader):
                         arrow_schema
                     ),
                     empty_metadata,
+                    schema=arrow_schema,
+                    per_task_row_limit=per_task_row_limit,
                 )
             ]
         for index, _data_file in enumerate(data_files):
@@ -132,7 +155,8 @@ class _LakeSoulDatasourceReader(Reader):
                 partitions=self._partitions,
                 retain_partition_columns=self._retain_partition_columns,
                 # entry
-                namespace=self._namespace: _read_lakesoul_data_file(
+                namespace=self._namespace,
+                object_store_configs=self._object_store_configs: _read_lakesoul_data_file(
                     table_name=table_name,
                     batch_size=batch_size,
                     thread_count=thread_count,
@@ -141,34 +165,14 @@ class _LakeSoulDatasourceReader(Reader):
                     partitions=partitions,
                     retain_partition_columns=retain_partition_columns,
                     namespace=namespace,
+                    object_store_configs=object_store_configs,
                 ),
                 metadata,
+                schema=arrow_schema,
+                per_task_row_limit=per_task_row_limit,
             )
             read_tasks.append(read_task)
         return read_tasks
-
-
-class LakeSoulDatasource(Datasource):
-    def create_reader(
-        self,
-        table_name: str,
-        batch_size: int = 16,
-        thread_count: int = 1,
-        partitions: Optional[Mapping[str, str]] = None,
-        retain_partition_columns: bool = False,
-        namespace: str = "default",
-    ) -> Reader:
-        normalized_partitions: Optional[Dict[str, str]] = (
-            dict(partitions) if partitions is not None else None
-        )
-        return _LakeSoulDatasourceReader(
-            table_name=table_name,
-            batch_size=batch_size,
-            thread_count=thread_count,
-            partitions=normalized_partitions,
-            retain_partition_columns=retain_partition_columns,
-            namespace=namespace,
-        )
 
     def do_write(self, *args: Any, **kwargs: Any) -> None:
         message = "write to LakeSoul is not implemented yet"
@@ -182,22 +186,31 @@ def read_lakesoul(
     partitions: Optional[Mapping[str, str]] = None,
     retain_partition_columns: bool = False,
     namespace: str = "default",
+    object_store_configs: Optional[Mapping[str, str]] = None,
 ) -> Dataset:
     arrow_schema = get_arrow_schema_by_table_name(
         table_name=table_name,
         namespace=namespace,
         retain_partition_columns=retain_partition_columns,
     )
-    ds = ray.data.read_datasource(
-        LakeSoulDatasource(),
-        table_name=table_name,
-        batch_size=batch_size,
-        thread_count=thread_count,
-        partitions=partitions,
-        retain_partition_columns=retain_partition_columns,
-        namespace=namespace,
+    normalized_partitions: Optional[Dict[str, str]] = (
+        dict(partitions) if partitions is not None else None
     )
-    ds._plan.cache_schema(arrow_schema)
+    normalized_object_store_configs: Dict[str, str] = (
+        dict(object_store_configs) if object_store_configs is not None else {}
+    )
+    ds = ray.data.read_datasource(
+        LakeSoulDatasource(
+            table_name=table_name,
+            batch_size=batch_size,
+            thread_count=thread_count,
+            partitions=normalized_partitions,
+            retain_partition_columns=retain_partition_columns,
+            namespace=namespace,
+            object_store_configs=normalized_object_store_configs,
+        )
+    )
+    _cache_dataset_schema(ds, arrow_schema)
     return ds
 
 
