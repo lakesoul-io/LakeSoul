2b4400e7b9a1404325625ae5b1d45c2f777ee1d8
chenxu <chenxu@dmetasoul.com>
2026-07-03 10:04:09 +0800

    fix tests env
    Signed-off-by: chenxu <chenxu@dmetasoul.com>

--- docker/lakesoul-docker-compose-env/mysql_init.sql
+++ docker/lakesoul-docker-compose-env/mysql_init.sql
@@ -8,7 +8,7 @@ use test_cdc;
 
 create table `default_init`
 (
-    `id`     bigint(20)     AUTO_INCREMENT NOT NULL,
+    `id`     int            NOT NULL,
     `col_1`  bigint         NOT NULL,
     `col_2`  binary(1)      NOT NULL,
     `col_3`  blob           NOT NULL,
