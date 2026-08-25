05930f3781eb0bf1872688620d30ff75ec62a6b4
Xu Chen <xuchen-plus@users.noreply.github.com>
2026-08-25 16:50:10 +0800

    fix: allow vortex file access in s3 proxy (#862)
    * fix: allow vortex file access in s3 proxy
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    
    * add test case
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    
    ---------
    
    Signed-off-by: chenxu <chenxu@dmetasoul.com>
    Co-authored-by: chenxu <chenxu@dmetasoul.com>

--- rust/lakesoul-s3-proxy/src/main.rs
+++ rust/lakesoul-s3-proxy/src/main.rs
@@ -679,7 +679,8 @@ where
     split
         .take_while(|s| {
             !(s.ends_with(".parquet")
-                || s.starts_with("compact")
+                || s.ends_with(".vortex")
+                || s.starts_with("compactdir")
                 || s.contains(partition_equal))
         })
         .for_each(|s| {
@@ -848,6 +849,15 @@ mod tests {
                 "lakesoul-test-bucket"
             ),
             "s3://lakesoul-test-bucket/test/default/abc"
+        );
+        assert_eq!(
+            parse_table_path(
+                &Uri::from_static(
+                    "/lakeinsight-bucket/lakeinsight-v1/warehouse/default/test_table/part--0001-89bb5bc9-ad96-4906-9001-1d33a06a675e-c000.vortex"
+                ),
+                "lakeinsight-bucket"
+            ),
+            "s3://lakeinsight-bucket/lakeinsight-v1/warehouse/default/test_table"
         );
     }
 
